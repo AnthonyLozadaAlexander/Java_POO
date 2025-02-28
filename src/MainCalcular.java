21ae4dc3109ee76f0f3f09fc8ee5810e07174d6a
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-28 00:15:27 -0500

    commit update

---
+++ src/MainCalcular.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MainCalcular {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Creacion Del Objeto e Instancia
+        Rectangulo datos = new Rectangulo();
+
+        // Ingreso De Datos A Los Atributos Del Objeto
+        System.out.println("---------------------------------------------");
+        System.out.print("\tIngrese la base del rectangulo: ");
+        datos.base = input.nextFloat();
+        System.out.print("\tIngrese la altura del rectangulo: ");
+        datos.altura = input.nextFloat();
+        System.out.println("---------------------------------------------");
+
+        datos.mostrarArea();
+    }
+}
---
+++ src/Rectangulo.java
@@ -0,0 +1,17 @@
+public class Rectangulo {
+    // Atributos
+    public float base, altura;
+
+    //Metodo constructor
+    public Rectangulo(){
+        base = 0;
+        altura = 0;
+    }
+
+    public void mostrarArea(){
+
+        System.out.println("---------------------------------------------");
+        System.out.println("\tEl area del rectangulo es: " + (base * altura));
+        System.out.println("---------------------------------------------");
+    }
+}
