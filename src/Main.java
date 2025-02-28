7444040d348d1a46aacf29c9d9e58a2dbeca674e
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-27 23:48:54 -0500

    commit update

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+       //Creacion De Objeto A Partir De La Clase Persona.java
+       Persona persona1 = new Persona();
+       persona1.nombre = "Anthony";
+       persona1.apellidos = "Lozada";
+       persona1.edad = 20;
+       persona1.mostrar();
+
+       // Nuevo Objeto A Partir De Clase La Clase Persona.java
+       Persona persona2 = new Persona();
+       persona2.nombre = "Carla";
+       persona2.apellidos = "Rodriguez";
+       persona2.edad = 19;
+       persona2.mostrar();
+
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+    //Atributos
+    String nombre;
+    String apellidos;
+    byte edad;
+
+    //Constructor
+    Persona(){
+        System.out.println("\nMi primer POO");
+    }
+
+    //Comportamientos o Metodos
+    public void mostrar(){
+        System.out.println(nombre + " " + apellidos + " Buenos Dias");
+    }
+}
