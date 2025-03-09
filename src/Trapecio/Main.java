f20d7d2960d7a3f9cae18b6ae542904ba8da1603
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-09 02:26:13 -0500

    Add Main and Trapecio classes for trapezoid area calculation

---
+++ src/Trapecio/Main.java
@@ -0,0 +1,24 @@
+package Trapecio;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float baseMayor, baseMenor, altura;
+
+        System.out.print("Ingrese la base mayor -> ");
+        baseMayor = input.nextFloat();
+        System.out.print("Ingrese la base menor -> ");
+        baseMenor = input.nextFloat();
+        System.out.print("Ingrese la altura -> ");
+        altura = input.nextFloat();
+
+        Trapecio datosTrapecio = new Trapecio(); // Instancia de la clase Trapecio y creacion de un objeto de la clase Trapecio
+
+        datosTrapecio.setBase_Mayor(baseMayor);
+        datosTrapecio.setBase_Menor(baseMenor);
+        datosTrapecio.setAltura(altura);
+    }
+}
+
--- src/Trapecio/Trapecio.java
+++ src/Trapecio/Trapecio.java
@@ -1,4 +1,30 @@
 package Trapecio;
 
 public class Trapecio {
+
+    //Atributos del trapecio
+    private float base_Mayor;
+    private float base_Menor;
+    private float altura;
+
+    // Constructor
+    public Trapecio(){
+
+    }
+    // Metodos setters: nos permiten asignar datos a los atributos
+    public void setBase_Mayor(float base_Mayor){
+        this.base_Mayor = base_Mayor;
+    }
+
+    public void setBase_Menor(float base_Menor){
+        this.base_Menor = base_Menor;
+    }
+
+    public void setAltura(float altura){
+        this.altura = altura;
+    }
+
+    public void CalcularArea(){
+        System.out.println("El area del trapecio es: " + ((base_Mayor + base_Menor) * altura) / 2 + " cm^2");
+    }
 }
