831a549c2b2bb2e1498d62f9ff041ac47a69c4f2
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-10 00:44:40 -0500

    Add Main class for circle perimeter calculation

---
+++ src/PerimetroCirculo/Main.java
@@ -0,0 +1,13 @@
+package PerimetroCirculo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circulo datosCirculo = new Circulo();
+        datosCirculo.setRadio(3);
+        datosCirculo.calcularPerimetro(); // Usando el valor de pi2 = 3.1416
+        // datosCirculo.setPi2(3.14);  actualizando el valor de pi2 a 3.14
+        System.out.println("-------------------------------------");
+        // datosCirculo.calcularPerimetro();  Usando el valor de pi2 = 3.14
+    }
+}
