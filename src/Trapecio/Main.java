da450f7a63975a21da8139a99fda9f446ba47364
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-09 21:32:28 -0500

    Calculate area in Trapecio class after setting attributes in Main

--- src/Trapecio/Main.java
+++ src/Trapecio/Main.java
@@ -16,9 +16,11 @@ public class Main {
 
         Trapecio datosTrapecio = new Trapecio(); // Instancia de la clase Trapecio y creacion de un objeto de la clase Trapecio
 
+        // Asignamiento de valores a los atributos de la clase Trapecio
         datosTrapecio.setBase_Mayor(baseMayor);
         datosTrapecio.setBase_Menor(baseMenor);
         datosTrapecio.setAltura(altura);
+        datosTrapecio.CalcularArea();
     }
 }
 
